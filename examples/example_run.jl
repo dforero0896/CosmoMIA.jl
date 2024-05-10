54b8850ea2c081f03341a91912bd1b57b46bf49e
Daniel Forero <daniel.forerosanchez@epfl.ch>
2024-05-10 16:55:40 +0200

    Added usage of small scale funcs to example

--- examples/example_run.jl
+++ examples/example_run.jl
@@ -27,19 +27,19 @@ function parse_commandline()
     s = ArgParseSettings()
     @add_arg_table s begin
         "--pos_path"
-        default = "/home/astro/dforero/projects/desi-patchy/run/BOXpos%sOM0.315OL0.685G360V2000.0_ALPTrs6.000z1.100.dat"
+        default = "/home/astro/dforero/projects/desi-patchy/run-ridged/BOXpos%sOM0.315OL0.685G360V2000.0_ALPTrs6.000z1.100.dat"
         required = false
         "--vel_path"
-        default = "/home/astro/dforero/projects/desi-patchy/run/VE%sEULz1.100.dat"
+        default = "/home/astro/dforero/projects/desi-patchy/run-ridged/VE%sEULz1.100.dat"
         required = false
         "--disp_path"
-        default = "/home/astro/dforero/projects/desi-patchy/run/PSI%sz1.100.dat"
+        default = "/home/astro/dforero/projects/desi-patchy/run-ridged/PSI%sz1.100.dat"
         required = false
         "--delta_path"
-        default = "/home/astro/dforero/projects/desi-patchy/run/deltaBOXOM0.315OL0.685G360V2000.0_ALPTrs6.000z1.100.dat"
+        default = "/home/astro/dforero/projects/desi-patchy/run-ridged/deltaBOXOM0.315OL0.685G360V2000.0_ALPTrs6.000z1.100.dat"
         required = false
         "--cweb_path"
-        default = "/home/astro/dforero/projects/desi-patchy/run/TwebDelta_OM0.315OL0.685G360V2000.0z1.100.dat"
+        default = "/home/astro/dforero/projects/desi-patchy/run-ridged/TwebDelta_OM0.315OL0.685G360V2000.0lthD0.050z1.100.dat"
         required = false
         "--ref_catalog"
         help = "Reference catalog if fitting is to be made"
@@ -49,16 +49,24 @@ function parse_commandline()
         action = :store_true
         "--plot"
         action = :store_true
+        "--skip_collapse"
+        action = :store_true
         "--redshift"
         default = 1.1
         "--box_size"
         default = 2000.
         "--vel_boost"
-        default = 12f0
+        default = 8f0
+        "--disp_smooth"
+        default = 0.7f0
+        "--step_size"
+        default = -10f0
         "--number_counts"
         default = "/home/astro/dforero/cosmo3d/desi/patchy/ELG/z1.100/calibration/n_mock.dat"
         "--params"
-        default = [6.681508955504605, 5.47808573538, 0.7534895556587489, 1.1209400930989315, 0.10218062957157581]
+        #default = [5.38945528422064, 5.343785994677935, 0.0003635007999382034, 0.3902633911503999, 0.3761037446458434]
+        default = [6.7, 1., 0.4, 0.1, 0.2]
+        arg_type = Float64
         nargs = 5
         "--output_path"
         required = false
@@ -75,7 +83,7 @@ function fit_reference(args)
     box_min = @SVector [0.0f0 for _ in 1:3]
     grid_size = Tuple(360 for _ in 1:3)
     k_edges = range(3f-3, 0.505, step = 5f-3)
-    s_edges = Float32.(10 .^range(1f-1, log10(20), 51))
+    s_edges = Float32.(10 .^range(-1, log10(20), 51))
     los = [0,0,1]
     ref_size = 360
     bin_size = box_size ./ ref_size
@@ -99,7 +107,7 @@ function fit_reference(args)
         p0 = plot(k, k .^ 1.0 .* real.(@view(power_ref_rsd[1, :])), xscale=:identity, ylabel=L"P_z(k)", xlabel=L"k~[h / \mathrm{Mpc}]", label="Ref. l=0")
         p0 = plot!(p0, k, k .^ 1.0 .* real.(@view(power_ref_rsd[2, :])), xscale=:identity, ylabel=L"P_z(k)", xlabel=L"k~[h / \mathrm{Mpc}]", label="Ref. l=2")
         p0 = plot!(p0, k, k .^ 1.0 .* real.(@view(power_ref_rsd[3, :])), xscale=:identity, ylabel=L"P_z(k)", xlabel=L"k~[h / \mathrm{Mpc}]", label="Ref. l=4")
-        px = plot(s, s .^ 2 .* xi_ref, xscale = :log10, label = "Ref.", xlabel = L"s~[{\rm  Mpc}/h]", ylabel = L"s^2\xi(s)")
+        px = plot(s, s .^ 0 .* xi_ref, xscale = :log10, yscale = :log10, label = "Ref.", xlabel = L"s~[{\rm  Mpc}/h]", ylabel = L"\xi(s)")
     end #if
     println("Creating reference field")
     ref_field = zeros(Float32, ref_size, ref_size, ref_size)
@@ -123,12 +131,26 @@ function fit_reference(args)
     
 
     patchy_field = permutedims(reshape(read!(args["number_counts"], Array{Float32}(undef, ref_size^3)), (ref_size, ref_size, ref_size)), (3,2,1))
+
+    println("Getting a small-scale displacement")
+    CosmoMIA.filter_displacement!(alpt_storage.disp_field[1], args["disp_smooth"], box_size)
+    CosmoMIA.filter_displacement!(alpt_storage.disp_field[2], args["disp_smooth"], box_size)
+    CosmoMIA.filter_displacement!(alpt_storage.disp_field[3], args["disp_smooth"], box_size)
+
+    
+
     patchy_field = UInt32.(round.(patchy_field))
 
     params = args["params"]
 
     Random.seed!(0)
     @time subgrid_cat = CosmoMIA.assign_particles_to_gals(alpt_storage, patchy_field, box_size, box_min, Normal(0, 0.6 * bin_size[1]); debug=false)
+    
+    @show reinterpret(SVector{3,eltype(subgrid_cat.pos)}, subgrid_cat.pos[:])[1]
+    CosmoMIA.apply_displacement!(subgrid_cat.pos, alpt_storage.disp_field, args["step_size"], box_size, box_min)
+    @show reinterpret(SVector{3,eltype(subgrid_cat.pos)}, subgrid_cat.pos[:])[1]
+
+
     alpt_storage = nothing
     
     collapsed_cat = CosmoMIA.copy_output(subgrid_cat)
@@ -198,7 +220,7 @@ function fit_reference(args)
         k, power_real, power_rsd, s, xi_rsd                                     
     end #func
 
-    k, power_real, power_rsd, s, xi_rsd = full_model(params, subgrid_cat, collapsed_cat, do_collapse = true, compute_power = true, compute_xi = true)
+    k, power_real, power_rsd, s, xi_rsd = full_model(params, subgrid_cat, collapsed_cat, do_collapse = !args["skip_collapse"], compute_power = true, compute_xi = true)
 
     if args["plot"]
         p = plot!(p, k, k .^ 1.0 .* real.(@view(power_real[1, :])), xscale=:identity, ylabel=L"P(k)", xlabel=L"k~[h / \mathrm{Mpc}]", label="Coll.")
@@ -213,7 +235,7 @@ function fit_reference(args)
         p0p = plot!(p0p, k, 100 .* (real.(@view(power_rsd[2, :])) ./ real.(@view(power_ref_rsd[2, :])) .- 1), label="Coll", ylabel=L"\frac{P}{P_{\mathrm{ref}}} -1 (\%)", ylim=(-5, 5))
         p0p = plot!(p0p, k, zero(k), ribbon=1, label=L"1\%", alpha=0.5)
 
-        px = plot!(px, s, s.^ 2 .* xi_rsd, label = "Coll,")
+        px = plot!(px, s, s.^ 0 .* xi_rsd, label = "Coll,")
         pxp = plot(s, 100 .* (xi_rsd ./ xi_ref .- 1.))
         pxp = plot!(pxp, s, zero(s), ribbon=1, label=L"1\%", alpha=0.5, ylims = (-5, 5), xscale = :log10)
         final_plot = plot(p, p0, pp, p0p,px, pxp,
@@ -226,7 +248,10 @@ function fit_reference(args)
                       dpi=300, 
                       size = (800, 800)
                       )
-        savefig(final_plot, "plots/example_run.png")
+        savefig(final_plot, "plots/cosmoMIA_run.png")
+    end #if
+    if args["skip_collapse"]
+        exit()
     end #if
 
     function loss(params; train_power = true, train_xi = true)
@@ -246,18 +271,19 @@ function fit_reference(args)
             real_loss = 0.
         end #if
         if train_xi
-            xi_loss = mean(abs, s.^2 .* (xi_rsd .- xi_ref))
+            #xi_loss = mean(abs, s.^2 .* (xi_rsd .- xi_ref))
+            xi_loss = mean(abs, (log10.(xi_rsd) .- log10.(xi_ref)))
         else
             xi_loss = 0.
         end #of
-        total_loss = 3. * mono_loss + 1.5 * quad_loss + 0.5 * real_loss + 50 * xi_loss
+        total_loss = 3. * mono_loss + 1.5 * quad_loss + 0.5 * real_loss + 50. * xi_loss
 
         println("Total loss: $total_loss with params $params")
         
         total_loss
     end #func
     println("==> Starting optimization...")
-    res = optimize((pars,) -> loss(pars; train_power = false, train_xi = true), params, NelderMead(), Optim.Options(show_trace = true))
+    res = optimize((pars,) -> loss(pars; train_power = true, train_xi = true), params, NelderMead(), Optim.Options(show_trace = true))
     @show Optim.converged(res)
     params = Optim.minimizer(res)
     @show loss(Optim.minimizer(params))
